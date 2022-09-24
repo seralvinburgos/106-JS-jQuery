8f7c9152532becf60308607d8bed8c3492e3ac09
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2022-09-24 16:38:09 -0400

    Task Manager update
    functions working properly as demonstrated from class. Bootstrap, CSS elements updated. Assignment 3 submission.

--- TaskManager/script/app.js
+++ TaskManager/script/app.js
@@ -4,8 +4,61 @@ var isImportant = false;
 var notVisible = true;
 
 function saveTask() {
-    console.log("Button clicked");
+    let title = $("#txtTitle").val();
+    let dueDate = $("#txtDueDate").val();
+    let category = $("#txtCategory").val();
+    let tag = $("#txtTag").val();
+    let color = $("#txtColor").val();
+    let description = $("#txtDescription").val();
+
+    let task = new Task(title, dueDate, category, tag, color, description);
+    displayTask(task);
+    clearForm();
 }
+
+function clearForm() {
+    $("#txtTitle").val("");
+    $("#txtDueDate").val("");
+    $("#txtCategory").val("selected");
+    $("#txtTag").val("");
+    $("#txtColor").val("");
+    $("#txtDescription").val("");
+}
+
+// create a displayTask function
+// the fn should receive a task parameter
+// and console log the parameter
+
+function displayTask(task) {
+    console.log(task);
+
+    let syntax = `
+    <div class="container d-flex flex-row justify-content-between align-items-stretch align-items-center rounded-3 shadow">
+        <div class="task-1 col-6 d-flex flex-column flex-fill rounded-3">
+            <div class="text-start mt-3 ms-2"><h3>${task.title}</h3> </div>
+            <br>
+            <div class="text-wrap text-start ms-2"><label>${task.description}</label></div>
+        </div>
+        <div class="task-2 col-2 d-flex flex-column flex-fill my-auto">
+            <div class="fst-italic"><h6>Due Date</h6></div>
+            <div class=""><label>${task.dueDate}</label></div>
+            <br>
+            <div class="fst-italic"><h6>Color</h6></div>
+            <div class=""><label>${task.color}</label></div>
+        </div>
+        <div class="task-3 col-2 d-flex flex-column flex-fill my-auto">
+            <div class="fst-italic"><h6>Category:</h6></div>
+            <div class=""><label>${task.category}</label></div>
+            <br>
+            <div class="fst-italic"><h6>Tag:</h6></div>
+            <div class=""><label>${task.tag}</label></div>
+        </div>
+    </div>
+    `;
+
+    $("#taskList").append(syntax);
+}
+
 function changeIcon() {
     if (!isImportant) {
         // change to important
---
+++ TaskManager/script/task.js
@@ -0,0 +1,14 @@
+
+class Task {
+
+    constructor(title, dueDate, cat, tag, color, desc) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.category = cat;
+        this.tag = tag;
+        this.color = color;
+        this.description = desc;
+
+        this.name = "Alvin";
+    }
+}
