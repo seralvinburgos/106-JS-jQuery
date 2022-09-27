90920aeaa0845c4fd31c44f7884ae54de5a63640
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2022-09-26 23:00:58 -0400

    TaskManager (submission)
    Finalized with working functions. Will submit for Competency Report.

--- TaskManager/script/app.js
+++ TaskManager/script/app.js
@@ -12,14 +12,30 @@ function saveTask() {
     let description = $("#txtDescription").val();
 
     let task = new Task(title, dueDate, category, tag, color, description);
-    displayTask(task);
-    clearForm();
+
+    // save on server
+    $.ajax({
+        type: "POST",
+        url: "https://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(task),
+        contentType: "application/json",
+        success: function(response) {
+            displayTask(task);
+            clearForm();
+        },
+        error: function(details) {
+            console.log("Save failed", details);
+            alert("Error, we could not save your task:);")
+        },
+    });
+
+
 }
 
 function clearForm() {
     $("#txtTitle").val("");
     $("#txtDueDate").val("");
-    $("#txtCategory").val("selected");
+    $("#txtCategory").val("");
     $("#txtTag").val("");
     $("#txtColor").val("");
     $("#txtDescription").val("");
@@ -80,10 +96,47 @@ function toggleSection() {
     }
 }
 
+function testRequest() {
+    //this is a test http request
+    $.ajax({
+        type: "GET",
+        url: "https://fsdiapi.azurewebsites.net/",
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(details) {
+            console.log("Error", details);
+        }
+    });
+}
+
+function fetchTasks() {
+    $.ajax({
+        type: "GET",
+        url: "https://fsdiapi.azurewebsites.net/api/tasks",
+        success: function(response) {
+            let allTasks = JSON.parse(response); // decode from json str to js object/array
+
+            // travel the array,
+            //get every task and send it to displayTask fn
+            for(let i=0; i<allTasks.length; i++) {
+                let task = allTasks[i];
+                if(task.name === "Alvin"){
+                    displayTask(task);
+                }                
+            }
+        },
+        error: function(details) {
+            console.log("Error", details);
+        }
+    });
+}
+
 function init() {
     console.log("Task Manager");
 
     // on init, we load previous data
+    fetchTasks();
 
     $(".info").hide();
     // catch events
@@ -93,9 +146,15 @@ function init() {
 
 }
 
-
-
-
-
-
 window.onload = init;
+
+/**
+ * 
+ * create the fetchTasks function
+ * send a get request to 'https://fsdiapi.azurewebsites.net/api/tasks'
+ * console.log the response from the server
+ * 
+ * 
+ * to clear your tasks from the database
+ * send a DELETE request to https://fsdiapi.azurewebsites.net/api/tasks/clear/Alvin
+ */
